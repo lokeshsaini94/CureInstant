5384775575afb119c221dfc3c3bda0d6475fa80b
lokeshsaini94 <lokeshsaini94@gmail.com>
2017-02-15 17:22:03 +0530

    Added fab in ReadFragment
    added fab in ReadFragment and fixed ic_add icons

--- app/src/main/java/com/cureinstant/cureinstant/fragments/ReadFragment.java
+++ app/src/main/java/com/cureinstant/cureinstant/fragments/ReadFragment.java
@@ -2,6 +2,8 @@ package com.cureinstant.cureinstant.fragments;
 
 
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.view.ViewPager;
@@ -53,6 +55,15 @@ public class ReadFragment extends Fragment {
             @Override
             public void onTabReselected(TabLayout.Tab tab) {
 
+            }
+        });
+
+        FloatingActionButton fab = (FloatingActionButton) rootView.findViewById(R.id.read_fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
             }
         });
 
