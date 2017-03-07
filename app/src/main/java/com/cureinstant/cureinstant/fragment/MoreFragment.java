2c7c4b281942fb33594e7ffc9c3a682d51ea15e8
lokeshsaini94 <lokeshsaini94@gmail.com>
2017-03-08 04:49:01 +0530

    added profile activity with basic structure

--- app/src/main/java/com/cureinstant/cureinstant/fragment/MoreFragment.java
+++ app/src/main/java/com/cureinstant/cureinstant/fragment/MoreFragment.java
@@ -1,6 +1,7 @@
 package com.cureinstant.cureinstant.fragment;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -8,12 +9,13 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.cureinstant.cureinstant.R;
+import com.cureinstant.cureinstant.activity.ProfileActivity;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class MoreFragment extends Fragment {
+public class MoreFragment extends Fragment implements View.OnClickListener {
 
 
     public MoreFragment() {
@@ -26,7 +28,20 @@ public class MoreFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View rootView = inflater.inflate(R.layout.fragment_more, container, false);
+
+        View profileView = rootView.findViewById(R.id.user_details_container);
+        profileView.setOnClickListener(this);
         return rootView;
     }
 
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.user_details_container:
+                Intent profileIntent = new Intent(getContext(), ProfileActivity.class);
+                startActivity(profileIntent);
+                break;
+        }
+
+    }
 }
