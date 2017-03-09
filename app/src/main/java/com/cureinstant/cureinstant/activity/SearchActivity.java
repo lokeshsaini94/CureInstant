5d661932abb0c15d30947fe6049a70d15967ce60
lokeshsaini94 <lokeshsaini94@gmail.com>
2017-03-09 21:35:41 +0530

    implement search activity close button

--- app/src/main/java/com/cureinstant/cureinstant/activity/SearchActivity.java
+++ app/src/main/java/com/cureinstant/cureinstant/activity/SearchActivity.java
@@ -1,15 +1,30 @@
 package com.cureinstant.cureinstant.activity;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageButton;
 
 import com.cureinstant.cureinstant.R;
 
-public class SearchActivity extends AppCompatActivity {
+public class SearchActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
+
+
+        ImageButton closeSearch = (ImageButton) findViewById(R.id.search_close);
+        closeSearch.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.search_close:
+                finish();
+                break;
+        }
     }
 }
