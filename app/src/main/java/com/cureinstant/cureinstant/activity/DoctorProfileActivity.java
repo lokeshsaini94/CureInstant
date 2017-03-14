fb5987e0358892260f5dce9f7b0233bfaac7ecc6
lokeshsaini94 <lokeshsaini94@gmail.com>
2017-03-14 14:02:17 +0530

    added doctor's profile layout

---
+++ app/src/main/java/com/cureinstant/cureinstant/activity/DoctorProfileActivity.java
@@ -0,0 +1,26 @@
+package com.cureinstant.cureinstant.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import com.cureinstant.cureinstant.R;
+
+public class DoctorProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_doctor_profile);
+
+        Toolbar bar = (Toolbar) findViewById(R.id.toolbar);
+        bar.setTitle(R.string.doctor_name);
+        bar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    }
+}
--- app/src/main/java/com/cureinstant/cureinstant/fragment/AppointmentFragment.java
+++ app/src/main/java/com/cureinstant/cureinstant/fragment/AppointmentFragment.java
@@ -1,6 +1,7 @@
 package com.cureinstant.cureinstant.fragment;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -8,6 +9,7 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.cureinstant.cureinstant.R;
+import com.cureinstant.cureinstant.activity.DoctorProfileActivity;
 
 
 /**
@@ -26,6 +28,15 @@ public class AppointmentFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View rootView = inflater.inflate(R.layout.fragment_appointment, container, false);
+
+        View docView = rootView.findViewById(R.id.doctor_info_card);
+        docView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getContext(), DoctorProfileActivity.class);
+                startActivity(intent);
+            }
+        });
         return rootView;
     }
 
