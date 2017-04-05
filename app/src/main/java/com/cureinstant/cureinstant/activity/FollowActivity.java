045f3ac8e0d6dec6fa545d57155d328b04e9d8dc
lokeshsaini94 <lokeshsaini94@gmail.com>
2017-04-05 13:19:04 +0530

    added load more followings and followers

--- app/src/main/java/com/cureinstant/cureinstant/activity/FollowActivity.java
+++ app/src/main/java/com/cureinstant/cureinstant/activity/FollowActivity.java
@@ -1,5 +1,6 @@
 package com.cureinstant.cureinstant.activity;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.DefaultItemAnimator;
@@ -11,10 +12,25 @@ import android.widget.Toast;
 
 import com.cureinstant.cureinstant.R;
 import com.cureinstant.cureinstant.adapter.FollowAdapter;
+import com.cureinstant.cureinstant.misc.OnLoadMoreListener;
 import com.cureinstant.cureinstant.misc.SimpleDividerItemDecoration;
 import com.cureinstant.cureinstant.model.Follow;
+import com.cureinstant.cureinstant.util.Utilities;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
 import java.util.ArrayList;
+
+import okhttp3.FormBody;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+
+import static com.cureinstant.cureinstant.util.Utilities.accessTokenValue;
 
 public class FollowActivity extends AppCompatActivity {
 
@@ -48,12 +64,153 @@ public class FollowActivity extends AppCompatActivity {
             }
         });
 
-
         followList = (RecyclerView) findViewById(R.id.follow_recycler_view);
         followList.setLayoutManager(new LinearLayoutManager(FollowActivity.this));
-        followAdapter = new FollowAdapter(FollowActivity.this, type, follows);
+        followAdapter = new FollowAdapter(FollowActivity.this, type, follows, followList);
         followList.setAdapter(followAdapter);
         followList.setItemAnimator(new DefaultItemAnimator());
         followList.addItemDecoration(new SimpleDividerItemDecoration(this));
+        followAdapter.setOnLoadMoreListener(new OnLoadMoreListener() {
+            @Override
+            public void onLoadMore() {
+                if (Utilities.checkConnection()) {
+                    RequestMoreFollowData requestMoreFollowData = new RequestMoreFollowData(type, follows.get(follows.size() - 1).getFollowID());
+                    requestMoreFollowData.execute();
+                }
+            }
+        });
     }
+
+    private class RequestMoreFollowData extends AsyncTask<Void, Void, ArrayList<Follow>> {
+
+        ArrayList<Follow> newFollows = new ArrayList<>();
+        private int oldFeedItemCount = follows.size() - 1;
+        private int addedFeedItemCount = 0;
+        private String type;
+        private int lastFollowID;
+
+        public RequestMoreFollowData(String type, int lastFollowID) {
+            this.type = type;
+            this.lastFollowID = lastFollowID;
+        }
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            newFollows.clear();
+            newFollows.addAll(follows);
+            //add null , so the adapter will check view_type and show progress bar at bottom
+            follows.add(null);
+            followAdapter.notifyItemInserted(follows.size() - 1);
+        }
+
+        @Override
+        protected ArrayList<Follow> doInBackground(Void... params) {
+            OkHttpClient client = new OkHttpClient();
+            int followID = 0;
+            int userID = 0;
+            String name = "";
+            String username = "";
+            String speciality = "";
+            String picture = "";
+            boolean isFollowing = false;
+
+            String url = "http://www.cureinstant.com/api/";
+            if (type.equals("followers")) {
+                url += "followers/load-more";
+            } else {
+                url += "followings/load-more";
+            }
+
+            RequestBody body = new FormBody.Builder()
+                    .add("id", String.valueOf(lastFollowID))
+                    .build();
+
+            Request request = new Request.Builder()
+                    .url(url)
+                    .header("Authorization", "Bearer " + accessTokenValue)
+                    .post(body)
+                    .build();
+            try {
+                Response response = client.newCall(request).execute();
+                String s = response.body().string();
+
+                follows.remove(follows.size() - 1); // Remove progressView
+
+                JSONObject followJson = new JSONObject(s);
+                JSONArray followArray;
+                if (type.equals("followers")) {
+                    followArray = followJson.getJSONArray("followers");
+                } else {
+                    followArray = followJson.getJSONArray("followings");
+                }
+
+                for (int i = 0; i < followArray.length(); i++) {
+                    followID = 0;
+                    userID = 0;
+                    isFollowing = false;
+                    name = "";
+                    username = "";
+                    speciality = "";
+                    picture = "";
+
+                    JSONObject followObject = followArray.getJSONObject(i);
+                    switch (type) {
+                        case "followers":
+                            followID = followObject.getInt("id");
+                            userID = followObject.getInt("f_id");
+                            isFollowing = followObject.getBoolean("following");
+                            JSONObject followerObject = followObject.getJSONObject("follower");
+                            name = followerObject.getString("name");
+                            username = followerObject.getString("username");
+                            if (!followerObject.isNull("speciality")) {
+                                speciality = followerObject.getString("speciality");
+                            }
+                            if (!followerObject.isNull("profile_pic")) {
+                                JSONObject pictureObject = followerObject.getJSONObject("profile_pic");
+                                picture = pictureObject.getString("pic_name");
+                            }
+                            break;
+                        default:
+                            followID = followObject.getInt("id");
+                            userID = followObject.getInt("f_id");
+                            JSONObject followingObject = followObject.getJSONObject("following");
+                            name = followingObject.getString("name");
+                            username = followingObject.getString("username");
+                            if (!followingObject.isNull("speciality")) {
+                                speciality = followingObject.getString("speciality");
+                            }
+                            if (!followingObject.isNull("profile_pic")) {
+                                JSONObject pictureObject = followingObject.getJSONObject("profile_pic");
+                                picture = pictureObject.getString("pic_name");
+                            }
+                            break;
+                    }
+
+                    newFollows.add(new Follow(followID, userID, name, username, speciality, picture, isFollowing));
+                }
+
+                return newFollows;
+
+            } catch (IOException | JSONException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(ArrayList<Follow> newFollows) {
+            super.onPostExecute(newFollows);
+            if (newFollows != null && !newFollows.isEmpty()) {
+                if (newFollows.size() > oldFeedItemCount) {
+                    follows.clear();
+                    follows.addAll(newFollows);
+                    followAdapter.setLoaded();
+                } else {
+                    followAdapter.notifyItemRemoved(follows.size() - 1);
+                }
+            }
+        }
+    }
+
 }
--- app/src/main/java/com/cureinstant/cureinstant/adapter/FollowAdapter.java
+++ app/src/main/java/com/cureinstant/cureinstant/adapter/FollowAdapter.java
@@ -3,17 +3,20 @@ package com.cureinstant.cureinstant.adapter;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
 import com.cureinstant.cureinstant.R;
 import com.cureinstant.cureinstant.activity.DoctorProfileActivity;
+import com.cureinstant.cureinstant.misc.OnLoadMoreListener;
 import com.cureinstant.cureinstant.model.Follow;
 import com.cureinstant.cureinstant.util.Utilities;
 
@@ -23,85 +26,135 @@ import java.util.ArrayList;
  * Created by lokeshsaini94 on 04-04-2017.
  */
 
-public class FollowAdapter extends RecyclerView.Adapter<FollowAdapter.ItemViewHolder> {
+public class FollowAdapter extends RecyclerView.Adapter {
+    private final int VIEW_ITEM = 1;
+    private final int VIEW_PROG = 0;
+
+    // The minimum amount of items to have below your current scroll position
+    // before loading more.
+    private int visibleThreshold = 5;
+    private int lastVisibleItem, totalItemCount;
+    private boolean loading;
+    private OnLoadMoreListener onLoadMoreListener;
 
     private Context context;
     private String type;
     private ArrayList<Follow> follows;
 
-    public FollowAdapter(Context context, String type, ArrayList<Follow> follows) {
+    public FollowAdapter(Context context, String type, ArrayList<Follow> follows, RecyclerView recyclerView) {
         this.context = context;
         this.type = type;
         this.follows = follows;
-    }
 
-    @Override
-    public ItemViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        if (recyclerView.getLayoutManager() instanceof LinearLayoutManager) {
 
-        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.layout_follow_profile, parent,
-                false);
-
-        return new ItemViewHolder(view);
-    }
-
-    @Override
-    public void onBindViewHolder(final ItemViewHolder holder, int position) {
-
-        holder.name.setText(follows.get(position).getName());
-        holder.speciality.setText(follows.get(position).getSpeciality());
-        String imageURL = Utilities.profilePicSmallBaseUrl + follows.get(position).getPicture();
-        Glide.with(context).load(imageURL).placeholder(R.drawable.doctor_placeholder).into(holder.picture);
-
-        if (type.equals("followers")) {
-            if (follows.get(position).isFollowing()) {
-                holder.followButton.setBackgroundColor(context.getResources().getColor(R.color.colorPrimary));
-                holder.followButton.setTextColor(context.getResources().getColor(R.color.white));
-                holder.followButton.setText("Unfollow");
-            } else {
-                holder.followButton.setBackgroundColor(context.getResources().getColor(R.color.white));
-                holder.followButton.setTextColor(context.getResources().getColor(R.color.colorPrimary));
-                holder.followButton.setText("Follow");
-            }
-        } else {
-            holder.followButton.setBackgroundColor(context.getResources().getColor(R.color.colorPrimary));
-            holder.followButton.setTextColor(context.getResources().getColor(R.color.white));
-            holder.followButton.setText("Unfollow");
-        }
+            final LinearLayoutManager linearLayoutManager = (LinearLayoutManager) recyclerView
+                    .getLayoutManager();
 
 
-        View.OnClickListener onClickListener = new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                switch (v.getId()) {
-                    case R.id.doctor_follow_button:
-                        if (type.equals("followers") && !follows.get(holder.getAdapterPosition()).isFollowing()) {
-                            Utilities.FollowDoctor followDoctor = new Utilities.FollowDoctor(true, follows.get(holder.getAdapterPosition()).getUserID());
-                            followDoctor.execute();
-                            holder.followButton.setBackgroundColor(context.getResources().getColor(R.color.colorPrimary));
-                            holder.followButton.setTextColor(context.getResources().getColor(R.color.white));
-                            holder.followButton.setText("Unfollow");
-                        } else {
-                            Utilities.FollowDoctor followDoctor = new Utilities.FollowDoctor(false, follows.get(holder.getAdapterPosition()).getUserID());
-                            followDoctor.execute();
-                            holder.followButton.setBackgroundColor(context.getResources().getColor(R.color.white));
-                            holder.followButton.setTextColor(context.getResources().getColor(R.color.colorPrimary));
-                            holder.followButton.setText("Follow");
+            recyclerView
+                    .addOnScrollListener(new RecyclerView.OnScrollListener() {
+                        @Override
+                        public void onScrolled(RecyclerView recyclerView,
+                                               int dx, int dy) {
+                            super.onScrolled(recyclerView, dx, dy);
+
+                            totalItemCount = linearLayoutManager.getItemCount();
+                            lastVisibleItem = linearLayoutManager
+                                    .findLastVisibleItemPosition();
+                            if (!loading
+                                    && totalItemCount <= (lastVisibleItem + visibleThreshold)) {
+                                // End has been reached
+                                // Do something
+                                if (onLoadMoreListener != null) {
+                                    onLoadMoreListener.onLoadMore();
+                                }
+                                loading = true;
+                            }
                         }
-                        break;
-                    default:
-                        Intent intentAnswer = new Intent(context, DoctorProfileActivity.class);
-                        Bundle b = new Bundle();
-                        b.putString("name", follows.get(holder.getAdapterPosition()).getName());
-                        b.putString("username", follows.get(holder.getAdapterPosition()).getUsername());
-                        intentAnswer.putExtras(b);
-                        context.startActivity(intentAnswer);
-                        break;
-                }
-            }
-        };
+                    });
+        }
+    }
 
-        holder.followButton.setOnClickListener(onClickListener);
-        holder.rootView.setOnClickListener(onClickListener);
+    @Override
+    public int getItemViewType(int position) {
+        return follows.get(position) != null ? VIEW_ITEM : VIEW_PROG;
+    }
+
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        if (viewType == VIEW_ITEM) {
+            View itemView = LayoutInflater.from(parent.getContext())
+                    .inflate(R.layout.layout_follow_profile, parent, false);
+            return new FollowAdapter.ItemViewHolder(itemView);
+        } else if (viewType == VIEW_PROG) {
+            View itemView = LayoutInflater.from(parent.getContext())
+                    .inflate(R.layout.layout_progressbar, parent, false);
+            return new FollowAdapter.ProgressViewHolder(itemView);
+        }
+        View itemView = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.layout_follow_profile, parent, false);
+
+        return new ItemViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(final RecyclerView.ViewHolder holder, int position) {
+        if (holder instanceof ItemViewHolder) {
+
+            ((ItemViewHolder) holder).name.setText(follows.get(position).getName());
+            ((ItemViewHolder) holder).speciality.setText(follows.get(position).getSpeciality());
+            String imageURL = Utilities.profilePicSmallBaseUrl + follows.get(position).getPicture();
+            Glide.with(context).load(imageURL).placeholder(R.drawable.doctor_placeholder).into(((ItemViewHolder) holder).picture);
+
+            if (!type.equals("followers") || follows.get(position).isFollowing()) {
+                ((ItemViewHolder) holder).followButton.setBackgroundColor(context.getResources().getColor(R.color.colorPrimary));
+                ((ItemViewHolder) holder).followButton.setTextColor(context.getResources().getColor(R.color.white));
+                ((ItemViewHolder) holder).followButton.setText("Unfollow");
+            } else {
+                ((ItemViewHolder) holder).followButton.setBackgroundColor(context.getResources().getColor(R.color.white));
+                ((ItemViewHolder) holder).followButton.setTextColor(context.getResources().getColor(R.color.colorPrimary));
+                ((ItemViewHolder) holder).followButton.setText("Follow");
+            }
+
+
+            View.OnClickListener onClickListener = new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    switch (v.getId()) {
+                        case R.id.doctor_follow_button:
+                            if (type.equals("followers") && !follows.get(holder.getAdapterPosition()).isFollowing()) {
+                                Utilities.FollowDoctor followDoctor = new Utilities.FollowDoctor(true, follows.get(holder.getAdapterPosition()).getUserID());
+                                followDoctor.execute();
+                                ((ItemViewHolder) holder).followButton.setBackgroundColor(context.getResources().getColor(R.color.colorPrimary));
+                                ((ItemViewHolder) holder).followButton.setTextColor(context.getResources().getColor(R.color.white));
+                                ((ItemViewHolder) holder).followButton.setText("Unfollow");
+                            } else {
+                                Utilities.FollowDoctor followDoctor = new Utilities.FollowDoctor(false, follows.get(holder.getAdapterPosition()).getUserID());
+                                followDoctor.execute();
+                                ((ItemViewHolder) holder).followButton.setBackgroundColor(context.getResources().getColor(R.color.white));
+                                ((ItemViewHolder) holder).followButton.setTextColor(context.getResources().getColor(R.color.colorPrimary));
+                                ((ItemViewHolder) holder).followButton.setText("Follow");
+                            }
+                            break;
+                        default:
+                            Intent intentAnswer = new Intent(context, DoctorProfileActivity.class);
+                            Bundle b = new Bundle();
+                            b.putString("name", follows.get(holder.getAdapterPosition()).getName());
+                            b.putString("username", follows.get(holder.getAdapterPosition()).getUsername());
+                            intentAnswer.putExtras(b);
+                            context.startActivity(intentAnswer);
+                            break;
+                    }
+                }
+            };
+
+            ((ItemViewHolder) holder).followButton.setOnClickListener(onClickListener);
+            ((ItemViewHolder) holder).rootView.setOnClickListener(onClickListener);
+
+        } else {
+            ((ProgressViewHolder) holder).progressBar.setIndeterminate(true);
+        }
     }
 
     @Override
@@ -109,8 +162,26 @@ public class FollowAdapter extends RecyclerView.Adapter<FollowAdapter.ItemViewHo
         return follows.size();
     }
 
+    public void setLoaded() {
+        this.notifyDataSetChanged();
+        loading = false;
+    }
 
-    class ItemViewHolder extends RecyclerView.ViewHolder {
+    public void setOnLoadMoreListener(OnLoadMoreListener onLoadMoreListener) {
+        this.onLoadMoreListener = onLoadMoreListener;
+    }
+
+
+    private static class ProgressViewHolder extends RecyclerView.ViewHolder {
+        ProgressBar progressBar;
+
+        ProgressViewHolder(View v) {
+            super(v);
+            progressBar = (ProgressBar) v.findViewById(R.id.progressBar1);
+        }
+    }
+
+    private static class ItemViewHolder extends RecyclerView.ViewHolder {
 
         View rootView;
         TextView name, speciality;
