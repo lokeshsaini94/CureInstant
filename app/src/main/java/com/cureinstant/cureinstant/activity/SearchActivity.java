839d4165a130cbf6102579511111a7ce08777326
lokeshsaini94 <lokeshsaini94@gmail.com>
2017-04-06 16:43:11 +0530

    added search loadMore with lots of code changes

--- app/src/main/java/com/cureinstant/cureinstant/activity/SearchActivity.java
+++ app/src/main/java/com/cureinstant/cureinstant/activity/SearchActivity.java
@@ -17,6 +17,7 @@ import android.widget.TextView;
 
 import com.cureinstant.cureinstant.R;
 import com.cureinstant.cureinstant.adapter.SearchAdapter;
+import com.cureinstant.cureinstant.misc.OnLoadMoreListener;
 import com.cureinstant.cureinstant.misc.SimpleDividerItemDecoration;
 import com.cureinstant.cureinstant.model.SearchProfile;
 import com.cureinstant.cureinstant.util.Utilities;
@@ -39,6 +40,8 @@ import static com.cureinstant.cureinstant.util.Utilities.accessTokenValue;
 public class SearchActivity extends AppCompatActivity implements View.OnClickListener {
 
     ProgressDialog progressDialog;
+    private int currentPage = 1, lastPage = 1;
+    private RecyclerView searchRecyclerView;
     private SearchAdapter searchAdapter;
     private ArrayList<SearchProfile> searchProfiles = new ArrayList<>();
     private EditText searchEditText;
@@ -68,12 +71,23 @@ public class SearchActivity extends AppCompatActivity implements View.OnClickLis
         });
 
         final LinearLayoutManager mLayoutManager = new LinearLayoutManager(this);
-        RecyclerView searchRecyclerView = (RecyclerView) findViewById(R.id.search_recycler_view);
+        searchRecyclerView = (RecyclerView) findViewById(R.id.search_recycler_view);
         searchRecyclerView.setLayoutManager(mLayoutManager);
-        searchAdapter = new SearchAdapter(this, searchProfiles);
+        searchAdapter = new SearchAdapter(this, searchProfiles, searchRecyclerView);
         searchRecyclerView.setAdapter(searchAdapter);
         searchRecyclerView.setItemAnimator(new DefaultItemAnimator());
         searchRecyclerView.addItemDecoration(new SimpleDividerItemDecoration(this));
+        searchAdapter.setOnLoadMoreListener(new OnLoadMoreListener() {
+            @Override
+            public void onLoadMore() {
+                if (currentPage <= lastPage) {
+                    if (Utilities.checkConnection()) {
+                        RequestSearchData requestSearchData = new RequestSearchData();
+                        requestSearchData.execute();
+                    }
+                }
+            }
+        });
     }
 
     private void performSearch() {
@@ -82,6 +96,7 @@ public class SearchActivity extends AppCompatActivity implements View.OnClickLis
             searchEditText.setError(getString(R.string.error_field_required));
         } else {
             Utilities.hideSoftKeyboard(SearchActivity.this);
+            progressDialog.show();
             if (!searchProfiles.isEmpty()) {
                 searchProfiles.clear();
                 searchAdapter.notifyDataSetChanged();
@@ -103,23 +118,30 @@ public class SearchActivity extends AppCompatActivity implements View.OnClickLis
         }
     }
 
+    // Requests search data
     private class RequestSearchData extends AsyncTask<Void, Void, ArrayList<SearchProfile>> {
 
+        ArrayList<SearchProfile> tempSearchProfiles = new ArrayList<>();
         private String searchString = "";
 
         @Override
         protected void onPreExecute() {
             super.onPreExecute();
-            progressDialog.show();
             searchString = searchEditText.getText().toString();
+            tempSearchProfiles.addAll(searchProfiles);
+            if (currentPage != 1) {
+                //add null , so the adapter will check view_type and show progress bar at bottom
+                searchProfiles.add(null);
+                searchAdapter.notifyItemInserted(searchProfiles.size() - 1);
+            }
         }
 
         @Override
         protected ArrayList<SearchProfile> doInBackground(Void... params) {
-            ArrayList<SearchProfile> newSearchProfiles = new ArrayList<>();
             OkHttpClient client = new OkHttpClient();
 
             RequestBody body = new FormBody.Builder()
+                    .add("page", String.valueOf(currentPage))
                     .add("term", searchString)
                     .build();
 
@@ -135,12 +157,14 @@ public class SearchActivity extends AppCompatActivity implements View.OnClickLis
                 Response response = client.newCall(request).execute();
                 String s = response.body().string();
                 JSONObject searchObject = new JSONObject(s);
-                JSONArray suggestionsArray = searchObject.getJSONArray("suggestions");
-                if (suggestionsArray.length() < 1) {
+                JSONObject suggestionsObject = searchObject.getJSONObject("suggestions");
+                lastPage = suggestionsObject.getInt("last_page");
+                JSONArray dataArray = suggestionsObject.getJSONArray("data");
+                if (dataArray.length() < 1) {
                     return null;
                 } else {
-                    for (int i = 0; i < suggestionsArray.length(); i++) {
-                        JSONObject suggestion = suggestionsArray.getJSONObject(i);
+                    for (int i = 0; i < dataArray.length(); i++) {
+                        JSONObject suggestion = dataArray.getJSONObject(i);
                         int id = 0;
                         String name = "", username = "", speciality = "", picture = "";
 
@@ -155,9 +179,9 @@ public class SearchActivity extends AppCompatActivity implements View.OnClickLis
                             picture = pictureObject.getString("pic_name");
                         }
 
-                        newSearchProfiles.add(new SearchProfile(id, name, username, speciality, picture));
+                        tempSearchProfiles.add(new SearchProfile(id, name, username, speciality, picture));
                     }
-                    return newSearchProfiles;
+                    return tempSearchProfiles;
                 }
 
             } catch (IOException | JSONException e) {
@@ -168,8 +192,13 @@ public class SearchActivity extends AppCompatActivity implements View.OnClickLis
 
         @Override
         protected void onPostExecute(ArrayList<SearchProfile> newSearchProfiles) {
-            super.onPostExecute(searchProfiles);
+            super.onPostExecute(newSearchProfiles);
             progressDialog.dismiss();
+            if (currentPage != 1) {
+                searchProfiles.remove(searchProfiles.size() - 1); // Remove progressView
+                searchAdapter.notifyItemRemoved(searchProfiles.size());
+            }
+            currentPage += 1;
             if (newSearchProfiles == null) {
                 final Snackbar snackBar = Snackbar.make(findViewById(R.id.search_root_view), "No results found", Snackbar.LENGTH_LONG);
                 snackBar.setAction("Dismiss", new View.OnClickListener() {
@@ -182,7 +211,8 @@ public class SearchActivity extends AppCompatActivity implements View.OnClickLis
             } else {
                 searchProfiles.clear();
                 searchProfiles.addAll(newSearchProfiles);
-                searchAdapter.notifyDataSetChanged();
+                searchRecyclerView.getRecycledViewPool().clear();
+                searchAdapter.setLoaded();
             }
         }
     }
--- app/src/main/java/com/cureinstant/cureinstant/adapter/SearchAdapter.java
+++ app/src/main/java/com/cureinstant/cureinstant/adapter/SearchAdapter.java
@@ -3,16 +3,19 @@ package com.cureinstant.cureinstant.adapter;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
 import com.cureinstant.cureinstant.R;
 import com.cureinstant.cureinstant.activity.DoctorProfileActivity;
+import com.cureinstant.cureinstant.misc.OnLoadMoreListener;
 import com.cureinstant.cureinstant.model.SearchProfile;
 import com.cureinstant.cureinstant.util.Utilities;
 
@@ -23,25 +26,79 @@ import java.util.ArrayList;
  */
 
 public class SearchAdapter extends RecyclerView.Adapter {
+    private final int VIEW_ITEM = 1;
+    private final int VIEW_PROG = 0;
+
+    // The minimum amount of items to have below your current scroll position
+    // before loading more.
+    private int visibleThreshold = 2;
+    private int lastVisibleItem, totalItemCount;
+    private boolean loading;
+    private OnLoadMoreListener onLoadMoreListener;
 
     private Context context;
     private ArrayList<SearchProfile> searchProfiles;
 
-    public SearchAdapter(Context context, ArrayList<SearchProfile> searchProfiles) {
+    public SearchAdapter(Context context, ArrayList<SearchProfile> searchProfiles, RecyclerView recyclerView) {
         this.context = context;
         this.searchProfiles = searchProfiles;
+
+        if (recyclerView.getLayoutManager() instanceof LinearLayoutManager) {
+
+            final LinearLayoutManager linearLayoutManager = (LinearLayoutManager) recyclerView
+                    .getLayoutManager();
+
+
+            recyclerView
+                    .addOnScrollListener(new RecyclerView.OnScrollListener() {
+                        @Override
+                        public void onScrolled(RecyclerView recyclerView,
+                                               int dx, int dy) {
+                            super.onScrolled(recyclerView, dx, dy);
+
+                            totalItemCount = linearLayoutManager.getItemCount();
+                            lastVisibleItem = linearLayoutManager
+                                    .findLastVisibleItemPosition();
+                            if (!loading
+                                    && totalItemCount <= (lastVisibleItem + visibleThreshold)) {
+                                // End has been reached
+                                // Do something
+                                if (onLoadMoreListener != null) {
+                                    onLoadMoreListener.onLoadMore();
+                                }
+                                loading = true;
+                            }
+                        }
+                    });
+        }
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        return searchProfiles.get(position) != null ? VIEW_ITEM : VIEW_PROG;
     }
 
     @Override
     public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.layout_search_profile, parent,
-                false);
+        if (viewType == VIEW_ITEM) {
+            View itemView = LayoutInflater.from(parent.getContext())
+                    .inflate(R.layout.layout_search_profile, parent, false);
+            return new SearchAdapter.ItemViewHolder(itemView);
+        } else if (viewType == VIEW_PROG) {
+            View itemView = LayoutInflater.from(parent.getContext())
+                    .inflate(R.layout.layout_progressbar, parent, false);
+            return new SearchAdapter.ProgressViewHolder(itemView);
+        }
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.layout_search_profile, parent,
+                        false);
 
         return new ItemViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(final RecyclerView.ViewHolder holder, final int position) {
+        if (holder instanceof ItemViewHolder) {
 
         ((ItemViewHolder) holder).doctorName.setText(searchProfiles.get(position).getName());
         ((ItemViewHolder) holder).doctorSpeciality.setText(searchProfiles.get(position).getSpeciality());
@@ -65,13 +122,34 @@ public class SearchAdapter extends RecyclerView.Adapter {
         };
 
         ((ItemViewHolder) holder).rootView.setOnClickListener(onClickListener);
+
+    } else {
+        ((ProgressViewHolder) holder).progressBar.setIndeterminate(true);
     }
+}
 
     @Override
     public int getItemCount() {
         return searchProfiles.size();
     }
 
+    public void setLoaded() {
+        this.notifyDataSetChanged();
+        loading = false;
+    }
+
+    public void setOnLoadMoreListener(OnLoadMoreListener onLoadMoreListener) {
+        this.onLoadMoreListener = onLoadMoreListener;
+    }
+
+    private static class ProgressViewHolder extends RecyclerView.ViewHolder {
+        ProgressBar progressBar;
+
+        ProgressViewHolder(View v) {
+            super(v);
+            progressBar = (ProgressBar) v.findViewById(R.id.progressBar1);
+        }
+    }
 
     private class ItemViewHolder extends RecyclerView.ViewHolder {
 
